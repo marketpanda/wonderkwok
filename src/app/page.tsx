ee4815f18be0adb5e52f3dfa0360c17b9f960e44
marketpanda <91251429+marketpanda@users.noreply.github.com>
2024-03-01 20:28:50 +0800

    layout fixed for frontpage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,17 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-gray-100">
-       <div className="flex flex-col">
-        <div><Image src="https://wonderwokbucket.s3.ap-southeast-1.amazonaws.com/wok.jpg" width={100} height={100} alt='Recipes' /></div>
-        <Link href='/recipes' className="rounded text-white bg-lime-500 px-3 py-2">
+       <div className="flex flex-col gap-2">
+        
+          <div className="rounded overflow-hidden">
+            <Image src="https://wonderwokbucket.s3.ap-southeast-1.amazonaws.com/wok.jpg" width={200} height={200} alt='Recipes' />
+          </div>
+            
+          
+          <Link href='/recipes' className="flex justify-center rounded text-white bg-lime-500 px-3 py-2">
 
           Wonderwok
-        </Link>
+          </Link>
        </div>
     </main>
   );
